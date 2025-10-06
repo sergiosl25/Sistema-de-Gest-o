aaed31feb8ce16c00e0a568b9d78d0fd2c8b93c8
sergiosl25 <sergio-alvessl@hotmail.com>
2025-10-06 12:45:20 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAKbGyqNjLGBPmPHaxCGvnDQV4tjQ",
   authDomain: "personalizados-2eb5f.firebaseapp.com",
   projectId: "personalizados-2eb5f",
-  storageBucket: "personalizados-2eb5f.firebasestorage.com",
+  storageBucket: "personalizados-2eb5f.firebasestorage.app",
   messagingSenderId: "498226923096",
   appId: "1:498226923096:web:98df6f34a7fd8630a5ec2d"
 };
@@ -20,5 +20,6 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 
 export { db, auth };
+
 
 
