eb4b4b29bac41fd638462c46619152a5fd5e7e67
sergiosl25 <sergio-alvessl@hotmail.com>
2025-10-06 13:13:38 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,7 @@
-import { initializeApp } from "firebase/app";
+// firebase-config.js
+import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAKbGyqNjLGBPmPHaxCGvnDQV4tjQWXFr8",
@@ -9,13 +12,15 @@ const firebaseConfig = {
   appId: "1:498226923096:web:98df6f34a7fd8630a5ec2d"
 };
 
-const app = initializeApp(firebaseConfig);
+// Inicializa o Firebase apenas se ainda não houver um app
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 
 // Exporta Firestore e Auth
 const db = getFirestore(app);
 const auth = getAuth(app);
 
 export { db, auth };
+
 
 
 
